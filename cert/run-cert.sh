046982a35ab1d3090b4e29a8337f8c96b6c706bd
Dolev <Hadad>
2024-07-06 18:16:04 +0300

    exp

---
+++ cert/run-cert.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DOM="www.nixcraft.com"
+PORT="443"
+FORCE_UPDATE=FALSE
+MIN_CERT_DATE=78
+now_epoch=$( date +%s )
+
+
+
+check(){
+    echo -n "$DOM:"
+    expiry_date=$( echo | openssl s_client -showcerts -servername $DOM -connect $DOM:$PORT 2>/dev/null | openssl x509 -inform pem -noout -enddate | cut -d "=" -f 2 )
+    echo -n " $expiry_date";
+    expiry_epoch=$( date -d "$expiry_date" +%s )
+    expiry_days="$(( ($expiry_epoch - $now_epoch) / (3600 * 24) ))"
+
+    echo "    $expiry_days days"
+
+    echo "expiry_days: "$expiry_days
+    if [ $MIN_CERT_DATE -gt "$expiry_days" ] ;then
+        echo "Cert was update"
+        update_cert
+    else
+        echo "Cert was not update"
+    fi
+}
+
+
+update_cert(){
+    echo "scp"
+}
+
+main(){
+  check
+}
+
+main 
--- cert/run.sh
+++ cert/run.sh
@@ -1,34 +1,75 @@
 #!/usr/bin/with-contenv bashio
 
 date 
+now_epoch=$( date +%s )
 
 if bashio::config.has_value 'CertLocation'; then
     echo $(bashio::config 'CertLocation')
 fi
+
 if bashio::config.has_value 'KeyLocation'; then
     echo $(bashio::config 'KeyLocation')
 fi
 
-if bashio::config.exists 'servers.server1' && bashio::config.exists 'servers.port1'; then
-    echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'servers.server1') -connect $(bashio::config 'servers.server1'):$(bashio::config 'servers.port1') \
-    | openssl x509 -noout -dates
-else 
-    echo "server1 not defined"
+if bashio::config.has_value 'MIN_CERT_DATE'; then
+    echo "MIN_CERT_DATE: " $(bashio::config 'MIN_CERT_DATE')
 fi
 
-if bashio::config.exists 'servers.server2' && bashio::config.exists 'servers.port2'; then
-    echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'servers.server2') -connect $(bashio::config 'servers.server2'):$(bashio::config 'servers.port2') \
-    | openssl x509 -noout -dates
-else 
-    echo "server2 not defined"
-fi
 
-if bashio::config.exists 'servers.server3' && bashio::config.exists 'servers.port3'; then
-    echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'servers.server3') -connect $(bashio::config 'servers.server3'):$(bashio::config 'servers.port3') \
-    | openssl x509 -noout -dates
-else 
-    echo "server3 not defined"
-fi
+start(){
+    if bashio::config.exists 'SERVERS.SERVER1' && bashio::config.exists 'SERVERS.PORT1'; then
+        echo "$(bashio::config 'SERVERS.SERVER1')"
+        DOM="$(bashio::config 'SERVERS.SERVER1')"
+        PORT="$(bashio::config 'SERVERS.PORT1')"
+        check
+    else 
+        echo "SERVER1 not defined"
+    fi
+
+    if bashio::config.exists 'SERVERS.SERVER2' && bashio::config.exists 'SERVERS.PORT2'; then
+        echo "$(bashio::config 'SERVERS.SERVER2')"
+        DOM="$(bashio::config 'SERVERS.SERVER2')"
+        PORT="$(bashio::config 'SERVERS.PORT2')"
+        check
+    else 
+        echo "SERVER2 not defined"
+    fi
+
+    if bashio::config.exists 'SERVERS.SERVER3' && bashio::config.exists 'SERVERS.PORT3'; then
+        echo "$(bashio::config 'SERVERS.SERVER3')"
+        DOM="$(bashio::config 'SERVERS.SERVER3')"
+        PORT="$(bashio::config 'SERVERS.PORT3')"
+        check
+    else 
+        echo "SERVER3 not defined"
+    fi
+}
+
+check(){
+    echo -n "$DOM:"
+    expiry_date=$( echo | openssl s_client -showcerts -servername $DOM -connect $DOM:$PORT 2>/dev/null | openssl x509 -inform pem -noout -enddate | cut -d "=" -f 2 )
+    echo -n " $expiry_date";
+    expiry_epoch=$( date -d "$expiry_date" +%s )
+    expiry_days="$(( ($expiry_epoch - $now_epoch) / (3600 * 24) ))"
+
+    echo "    $expiry_days days"
+
+    echo "expiry_days: "$expiry_days
+    if [ $MIN_CERT_DATE -gt "$expiry_days" ] ;then
+        echo "Cert was update"
+        update_cert
+    else
+        echo "Cert was not update"
+    fi
+}
+
+
+update_cert(){
+    echo "scp"
+}
+
+main(){
+  start
+}
+
+main 
