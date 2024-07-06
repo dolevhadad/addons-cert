07bc4d918eeb9d1cccb33855c2ae6dcd65c56a8b
Dolev <Hadad>
2024-07-06 17:03:14 +0300

    values

--- cert/run.sh
+++ cert/run.sh
@@ -9,25 +9,25 @@ if bashio::config.has_value 'KeyLocation'; then
     echo $(bashio::config 'KeyLocation')
 fi
 
-if bashio::config.exists 'Servers.server1' && bashio::config.exists 'Servers.port1'; then
+if bashio::config.exists 'servers.server1' && bashio::config.exists 'servers.port1'; then
     echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'Servers.server1') -connect $(bashio::config 'Servers.server1'):$(bashio::config 'Servers.port1') \
+    printf Q | openssl s_client -servername $(bashio::config 'servers.server1') -connect $(bashio::config 'servers.server1'):$(bashio::config 'servers.port1') \
     | openssl x509 -noout -dates
 else 
     echo "server1 not defined"
 fi
 
-if bashio::config.exists 'Servers.server2' && bashio::config.exists 'Servers.port2'; then
+if bashio::config.exists 'servers.server2' && bashio::config.exists 'servers.port2'; then
     echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'Servers.server2') -connect $(bashio::config 'Servers.server2'):$(bashio::config 'Servers.port2') \
+    printf Q | openssl s_client -servername $(bashio::config 'servers.server2') -connect $(bashio::config 'servers.server2'):$(bashio::config 'servers.port2') \
     | openssl x509 -noout -dates
 else 
     echo "server2 not defined"
 fi
 
-if bashio::config.exists 'Servers.server3' && bashio::config.exists 'Servers.port3'; then
+if bashio::config.exists 'servers.server3' && bashio::config.exists 'servers.port3'; then
     echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'Servers.server3') -connect $(bashio::config 'Servers.server3'):$(bashio::config 'Servers.port3') \
+    printf Q | openssl s_client -servername $(bashio::config 'servers.server3') -connect $(bashio::config 'servers.server3'):$(bashio::config 'servers.port3') \
     | openssl x509 -noout -dates
 else 
     echo "server3 not defined"
