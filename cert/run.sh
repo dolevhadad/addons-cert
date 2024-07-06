318410336d9ca6295ba9cdb936f74a7d95515f3a
Dolev <Hadad>
2024-07-06 15:06:00 +0300

    add values from config

--- cert/run.sh
+++ cert/run.sh
@@ -6,4 +6,11 @@ pwd
 echo "/"
 ls -l /
 cd /
+
+if bashio::config.has_value 'CertLocation'; then
+    echo $(bashio::config 'CertLocation')
+fi
+if bashio::config.has_value 'KeyLocation'; then
+    echo $(bashio::config 'KeyLocation')
+fi
 python3 -m http.server 8000
