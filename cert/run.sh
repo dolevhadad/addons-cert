66066feaf783bde6084885ca85cb68800fec1d3b
Dolev <Hadad>
2024-07-06 15:46:43 +0300

    fix

--- cert/run.sh
+++ cert/run.sh
@@ -8,7 +8,7 @@ if bashio::config.has_value 'KeyLocation'; then
     echo $(bashio::config 'KeyLocation')
 fi
 
-if [bashio::config.has_value 'ServerUrl'] && [bashio::config.has_value 'Port']; then
+if  bashio::config.has_value 'ServerUrl' && bashio::config.has_value 'Port'; then
     echo "$(bashio::config 'ServerUrl')"
     printf Q | openssl s_client -servername $(bashio::config 'ServerUrl') -connect $(bashio::config 'ServerUrl'):$(bashio::config 'Port') \
     | openssl x509 -noout -dates
