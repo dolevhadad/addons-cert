e0a23283e35992469fb71f07fccf598df172abdf
Dolev <Hadad>
2024-07-06 16:43:18 +0300

    values changes

--- cert/run.sh
+++ cert/run.sh
@@ -8,11 +8,26 @@ if bashio::config.has_value 'KeyLocation'; then
     echo $(bashio::config 'KeyLocation')
 fi
 
-if  bashio::config.has_value 'ServerUrl' && bashio::config.has_value 'Port'; then
+if bashio::config.exists 'servers.server1' && bashio::config.exists 'servers.port1'; then
     echo "$(bashio::config 'ServerUrl')"
-    printf Q | openssl s_client -servername $(bashio::config 'ServerUrl') -connect $(bashio::config 'ServerUrl'):$(bashio::config 'Port') \
+    printf Q | openssl s_client -servername $(bashio::config 'servers.server1') -connect $(bashio::config 'servers.server1'):$(bashio::config 'servers.port1') \
     | openssl x509 -noout -dates
 else 
-    echo "server not defined"
+    echo "server1 not defined"
 fi
 
+if bashio::config.exists 'servers.server2' && bashio::config.exists 'servers.port2'; then
+    echo "$(bashio::config 'ServerUrl')"
+    printf Q | openssl s_client -servername $(bashio::config 'servers.server2') -connect $(bashio::config 'servers.server2'):$(bashio::config 'servers.port2') \
+    | openssl x509 -noout -dates
+else 
+    echo "server2 not defined"
+fi
+
+if bashio::config.exists 'servers.server3' && bashio::config.exists 'servers.port3'; then
+    echo "$(bashio::config 'ServerUrl')"
+    printf Q | openssl s_client -servername $(bashio::config 'servers.server3') -connect $(bashio::config 'servers.server3'):$(bashio::config 'servers.port3') \
+    | openssl x509 -noout -dates
+else 
+    echo "server3 not defined"
+fi
